d06fe9fd5ac9a5472dc6b2740c92337a82ff171e
Adrien Bègue <adrien.begue@alten.com>
2020-04-20 15:40:37 +0200

    Update Diploma.java

--- src/main/java/com/alten/hercules/model/diploma/Diploma.java
+++ src/main/java/com/alten/hercules/model/diploma/Diploma.java
@@ -1,7 +1,5 @@
 package com.alten.hercules.model.diploma;
 
-import java.util.Date;
-
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -10,14 +8,12 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.MapsId;
 
-import com.alten.hercules.model.consultant.Consultant;
-
 @Entity
 public class Diploma {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	
+
 	private int graduationYear;
 	
 	@ManyToOne(fetch=FetchType.LAZY)
@@ -35,10 +31,15 @@ public class Diploma {
 		this.diplomaName = diplomaName;
 	}
 	
-	
-	
-	
-	
-	
-	
+	public Long getId() { return id; }
+	public void setId(Long id) { this.id = id; }
+
+	public int getGraduationYear() { return graduationYear; }
+	public void setGraduationYear(int graduationYear) { this.graduationYear = graduationYear; }
+
+	public DiplomaLocation getDiplomaLocation() { return diplomaLocation; }
+	public void setDiplomaLocation(DiplomaLocation diplomaLocation) { this.diplomaLocation = diplomaLocation; }
+
+	public DiplomaName getDiplomaName() { return diplomaName; }
+	public void setDiplomaName(DiplomaName diplomaName) { this.diplomaName = diplomaName; }
 }
