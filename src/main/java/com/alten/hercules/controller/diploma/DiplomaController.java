2f70203e96902fb74f38b9b804b953cdbc995d17
robin-jesson <62558641+robin-jesson@users.noreply.github.com>
2020-04-29 22:03:19 +0200

    Update DiplomaController.java

--- src/main/java/com/alten/hercules/controller/diploma/DiplomaController.java
+++ src/main/java/com/alten/hercules/controller/diploma/DiplomaController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,6 +27,7 @@ import com.alten.hercules.model.diploma.DiplomaName;
 import com.alten.hercules.model.diploma.Level;
 
 @RestController
+@CrossOrigin(origins="*")
 @RequestMapping("/hercules/diplomas")
 public class DiplomaController {
 	
