4eceafabb911725ccd395e6cc2780b8024928d0a
Adrien Bègue <adrien.begue@alten.com>
2020-04-30 15:30:37 +0200

    Modification modele mission

--- src/main/java/com/alten/hercules/controller/mission/http/response/MissionDetailsResponse.java
+++ src/main/java/com/alten/hercules/controller/mission/http/response/MissionDetailsResponse.java
@@ -13,36 +13,24 @@ import com.alten.hercules.model.mission.MissionSheet;
 
 public class MissionDetailsResponse {
 
-	private String city;
-	private Map<String, Object> consultant = new HashMap<String, Object>();
-	private String country;
-	private Map<String, Object> customer = new HashMap<String, Object>();
 	private Long id;
-	private ESheetStatus sheetStatus;
-	private Integer teamSize;
-	private EType type;
+	private Map<String, Object> consultant = new HashMap<String, Object>();
+	private Map<String, Object> customer = new HashMap<String, Object>();
 	private Set<MissionSheet> versions;
+	private ESheetStatus sheetStatus;
+	private EType type;
 	
 	public MissionDetailsResponse(Mission mission) {
-		this.city = mission.getCity();
-		fillConsultant(mission);
-		this.country = mission.getCountry();
-		fillCustomer(mission.getCustomer());
 		this.id = mission.getId();
-		this.sheetStatus = mission.getSheetStatus();
-		this.teamSize = mission.getTeamSize();
-		this.type = mission.getType();
+		fillConsultant(mission);
+		fillCustomer(mission.getCustomer());
 		this.versions = mission.getVersions();
+		this.sheetStatus = mission.getSheetStatus();
+		this.type = mission.getType();
 	}
-	
-	public String getCity() { return city; }
-	public void setCity(String city) { this.city = city; }
 	
 	public Map<String, Object> getConsultant() { return consultant; }
 	public void setConsultant(Map<String, Object> consultant) { this.consultant = consultant; }
-
-	public String getCountry() { return country; }
-	public void setCountry(String country) { this.country = country; }
 	
 	public Map<String, Object> getCustomer() { return customer; }
 	public void setCustomer(Map<String, Object> customer) { this.customer = customer; }
@@ -53,9 +41,6 @@ public class MissionDetailsResponse {
 	public ESheetStatus getSheetStatus() { return sheetStatus; }
 	public void setSheetStatus(ESheetStatus sheetStatus) { this.sheetStatus = sheetStatus; }
 
-	public Integer getTeamSize() { return teamSize; }
-	public void setTeamSize(Integer teamSize) { this.teamSize = teamSize; }
-
 	public EType getType() { return type; }
 	public void setType(EType type) { this.type = type; }
 	
@@ -64,7 +49,6 @@ public class MissionDetailsResponse {
 
 	private void fillConsultant(Mission mission) {
 		Consultant consultant = mission.getConsultant();
-		this.consultant.put("startXp", mission.getConsultantStartExp());
 		Map<String, String> diplomas = new HashMap<String, String>();
 		consultant.getDiplomas().forEach(diploma -> {
 			diplomas.put("city", diploma.getDiplomaLocation().getCity());
