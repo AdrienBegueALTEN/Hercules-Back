ad6a153a56c0683a09227e8109ff1f1bb8e0e069
Julien-BT <51945426+Julien-BT@users.noreply.github.com>
2020-05-05 16:21:13 +0200

    Correction de getAll et ajout de condition (details=true)

---
+++ src/main/java/com/alten/hercules/controller/mission/http/response/BasicMissionResponse.java
@@ -0,0 +1,48 @@
+package com.alten.hercules.controller.mission.http.response;
+
+import java.util.Set;
+
+
+import com.alten.hercules.model.consultant.Consultant;
+import com.alten.hercules.model.customer.Customer;
+import com.alten.hercules.model.mission.ESheetStatus;
+import com.alten.hercules.model.mission.Mission;
+import com.alten.hercules.model.mission.MissionSheet;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
+public class BasicMissionResponse {
+
+
+	private Long id;
+	@JsonIgnoreProperties(value = {"missions", "diplomas"})
+	private Consultant consultant;
+	@JsonIgnoreProperties(value = {"missions"})
+	private Customer customer;
+	private Set<MissionSheet> versions;
+	private ESheetStatus sheetStatus;
+	
+	public BasicMissionResponse(Mission mission) {
+		this.id = mission.getId();
+		this.consultant = mission.getConsultant();
+		this.customer = mission.getCustomer();
+		this.versions = mission.getVersions();
+		this.sheetStatus = mission.getSheetStatus();
+	}
+	
+	public Consultant getConsultant() { return consultant; }
+	public void setConsultant(Consultant consultant) { this.consultant = consultant; }
+	
+	public Customer getCustomer() { return customer; }
+	public void setCustomer(Customer customer) { this.customer = customer; }
+
+	public Long getId() { return id; }
+	public void setId(Long id) { this.id = id; }
+
+	public ESheetStatus getSheetStatus() { return sheetStatus; }
+	public void setSheetStatus(ESheetStatus sheetStatus) { this.sheetStatus = sheetStatus; }
+	
+	public Set<MissionSheet> getVersions() { return versions; }
+	public void setVersions(Set<MissionSheet> versions) { this.versions = versions; }
+}
+
