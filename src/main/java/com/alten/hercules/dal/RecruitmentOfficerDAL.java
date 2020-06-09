6520b34b4bc1300a6df07119cc8602114f856d92
Adrien Bègue <adrien.begue@alten.com>
2020-06-09 18:02:59 +0200

    Généralisation update manager

--- src/main/java/com/alten/hercules/dal/RecruitmentOfficerDAL.java
+++ src/main/java/com/alten/hercules/dal/RecruitmentOfficerDAL.java
@@ -1,13 +1,11 @@
 package com.alten.hercules.dal;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.alten.hercules.controller.consultant.http.response.BasicConsultantResponse;
 import com.alten.hercules.dao.consultant.ConsultantDAO;
 import com.alten.hercules.dao.user.RecruitmentOfficerDAO;
 import com.alten.hercules.dao.user.UserDAO;
@@ -24,13 +22,6 @@ public class RecruitmentOfficerDAL {
 	
 	@Autowired
 	private UserDAO userDAO;
-	
-	public List<BasicConsultantResponse> findAllEnabled() {
-		List<BasicConsultantResponse> consultants = new ArrayList<>();
-		consultantDAO.findByReleaseDateIsNull().forEach((consultant) -> {
-			consultants.add(new BasicConsultantResponse(consultant)); });
-		return consultants;
-	}
 	
 	public boolean emailIsAvailable(String email) {
 		return !(userDAO.existsByEmail(email) || consultantDAO.existsByEmail(email));
