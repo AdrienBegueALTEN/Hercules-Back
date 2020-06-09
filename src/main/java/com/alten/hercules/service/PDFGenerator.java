73b7836d88b36b5f63f17bf5d2e03f2693e54e5f
MaximeFoltz <65277730+MaximeFoltz@users.noreply.github.com>
2020-06-09 11:17:06 +0200

    début fiche projet PDF

--- src/main/java/com/alten/hercules/service/PDFGenerator.java
+++ src/main/java/com/alten/hercules/service/PDFGenerator.java
@@ -225,13 +225,16 @@ public class PDFGenerator {
         
         
         // Note de version
-        contentStream.setNonStrokingColor(black);
-        contentStream.beginText();
-        contentStream.newLineAtOffset(10, 10);
-        contentStream.setFont( font1, 10 );
-        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy"); 
-        contentStream.showText(dateFormat.format(mission.getLastVersion().getVersionDate()));
-        contentStream.endText();
+        if(mission.getLastVersion().getVersionDate()!=null) {
+	        contentStream.setNonStrokingColor(black);
+	        contentStream.beginText();
+	        contentStream.newLineAtOffset(10, 10);
+	        contentStream.setFont( font1, 10 );
+	        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy"); 
+	        contentStream.showText(dateFormat.format(mission.getLastVersion().getVersionDate()));
+	        contentStream.endText();
+        }
+        
         
         // Bulles
         
@@ -316,8 +319,286 @@ public class PDFGenerator {
         
 	}
 	
-	public static void makeProjectPDF(Project project,PDDocument document) throws FileNotFoundException, MalformedURLException {
+	public static void makeProjectPDF(Project project,PDDocument document) throws IOException {
 		
+		Mission mission = project.getMissionSheet().getMission();
+		
+		PDPage page = new PDPage(PDRectangle.A4);
+        page.setRotation(90);
+        document.addPage( page );
+
+        // Polices utilisées
+        PDFont font1 = PDType1Font.HELVETICA;
+        PDFont font2 = PDType1Font.HELVETICA_BOLD;
+        PDFont font3 = PDType1Font.HELVETICA_OBLIQUE;
+        
+        float height=page.getMediaBox().getHeight();
+        float width= page.getMediaBox().getWidth();
+        
+        // Images utilisées
+        PDImageXObject layoutAlten = PDImageXObject.createFromFile("src\\main\\resources\\pdflayout.png", document);
+        PDImageXObject blueStick = PDImageXObject.createFromFile("src\\main\\resources\\bluestick.png", document);
+        PDImageXObject contractIcon = PDImageXObject.createFromFile("src\\main\\resources\\contrat.png", document);
+        PDImageXObject teamIcon = PDImageXObject.createFromFile("src\\main\\resources\\equipe.png", document);
+        PDImageXObject durationIcon = PDImageXObject.createFromFile("src\\main\\resources\\duree.png", document);
+        PDImageXObject localizationIcon = PDImageXObject.createFromFile("src\\main\\resources\\localisation.png", document);
+        
+        
+        //Couleurs utilisées
+        PDColor white = new PDColor(new float[] { 1f, 1f, 1f }, PDDeviceRGB.INSTANCE);
+        PDColor black = new PDColor(new float[] { 0f, 0f, 0f }, PDDeviceRGB.INSTANCE);
+        PDColor darkblue = new PDColor(new float[] { 4/255f, 57/255f, 98/255f }, PDDeviceRGB.INSTANCE);
+        PDColor lightblue = new PDColor(new float[] { 0f, 139/255f, 210/255f }, PDDeviceRGB.INSTANCE);
+        
+        PDPageContentStream contentStream = new PDPageContentStream(document, page);
+        
+        
+        contentStream.transform(new Matrix(0, 1, -1, 0, width, 0));
+        
+        // en-tête
+        contentStream.drawImage(layoutAlten,0,0,height,width);
+        
+        if(project.getMissionSheet().getMission().getCustomer().getLogo()!=null) {
+	        PDImageXObject customerLogo = 
+	        		PDImageXObject.createFromFile("img\\logo\\"+mission.getCustomer().getLogo(), document);
+	        float optimalHeight = customerLogo.getHeight();
+	        float optimalWidth = customerLogo.getWidth();
+	        if(customerLogo.getWidth()>customerLogo.getHeight()) {
+	        	if(customerLogo.getWidth()>=65) {
+	        		optimalHeight = customerLogo.getHeight()/(customerLogo.getWidth()/65);
+	        		optimalWidth = 65;
+	        	}
+	        }
+	        else {
+	        	if(customerLogo.getHeight()>=65) {
+	        		optimalWidth = customerLogo.getWidth()/(customerLogo.getHeight()/65);
+	        		optimalHeight = 65;
+	        	}
+	        }
+	        
+	        contentStream.drawImage(customerLogo, height-70,width-70,optimalWidth,optimalHeight); //65 max en hauteur   65 max en largeur
+        }
+        
+        
+        contentStream.beginText();
+        contentStream.setFont( font1, 12 );
+        contentStream.setNonStrokingColor(white);
+        contentStream.newLineAtOffset(75, width-70);
+        contentStream.showText(project.getTitle());
+        contentStream.newLineAtOffset(0,35);
+        contentStream.setFont(font2, 18);
+        contentStream.setNonStrokingColor(darkblue);
+        contentStream.showText( "Projet");
+        contentStream.setFont(font1, 18);
+        contentStream.showText( " « "+ project.getMissionSheet().getConsultantRole()+
+        						" » chez "+ mission.getCustomer().getName()+ 
+        						" par "+ mission.getConsultant().getFirstname()+
+        						" "+mission.getConsultant().getLastname()+" " );
+        contentStream.endText();
+        
+        // description du client
+        contentStream.drawImage(blueStick,60,280,15,220);
+        contentStream.setNonStrokingColor(white);
+        contentStream.addRect(75, 280, 220, 220);
+        contentStream.fill();
+        contentStream.beginText();
+        contentStream.newLineAtOffset(85, 480);
+        contentStream.setFont( font1, 15 );
+        contentStream.setNonStrokingColor(lightblue);
+        contentStream.showText(mission.getCustomer().getName());
+        contentStream.newLineAtOffset(15, -20);
+        contentStream.setFont( font3, 10 );
+        contentStream.setNonStrokingColor(black);
+        contentStream.showText(mission.getCustomer().getActivitySector());
+        contentStream.setFont( font1, 10 );
+        contentStream.newLineAtOffset(0, -5);
+        
+        String customerDescription = mission.getCustomer().getDescription();
+        for(String line : separateLines(customerDescription,font1,185,10)) {
+        	contentStream.newLineAtOffset(0, -15);
+        	contentStream.showText(line);
+        }
+        contentStream.endText();
+                
+        // description du projet
+        contentStream.drawImage(blueStick,315,280,15,220);
+        contentStream.setNonStrokingColor(white);
+        contentStream.addRect(330, 280, 480, 220);
+        contentStream.fill();
+        contentStream.beginText();
+        contentStream.newLineAtOffset(340, 480);
+        contentStream.setFont( font1, 15 );
+        contentStream.setNonStrokingColor(lightblue);
+        contentStream.showText("La Mission ");
+        contentStream.newLineAtOffset(15, -5);
+        contentStream.setFont( font1, 10 );
+        contentStream.setNonStrokingColor(black);
+        
+        String projectDescription = project.getDescription();
+        for(String line : separateLines(projectDescription,font1,445,10)) {
+        	contentStream.newLineAtOffset(0, -15);
+        	contentStream.showText(line);
+        }
+        contentStream.endText();
+        
+        // diplômes du consultant
+        contentStream.drawImage(blueStick,315,80,15,180);
+        contentStream.setNonStrokingColor(white);
+        contentStream.addRect(330, 80, 220, 180);
+        contentStream.fill();
+        contentStream.beginText();
+        contentStream.newLineAtOffset(340, 240);
+        contentStream.setFont( font1, 15 );
+        contentStream.setNonStrokingColor(lightblue);
+        contentStream.showText(mission.getConsultant().getFirstname()+" "+mission.getConsultant().getLastname());
+        contentStream.newLineAtOffset(15, -5);
+        contentStream.setFont( font1, 10 );
+        contentStream.setNonStrokingColor(black);
+        
+        Set<Diploma> diplomas =  mission.getConsultant().getDiplomas();
+        for(Diploma diploma : diplomas ) {
+	        for(String line : separateLines(diploma.getEntitled(),font1,185,10)) {
+	        	contentStream.newLineAtOffset(0, -15);
+	        	contentStream.showText(line);
+	        }
+	        for(String line : separateLines(diploma.getEstablishment(),font1,185,10)) {
+	        	contentStream.newLineAtOffset(0, -15);
+	        	contentStream.showText(line);
+	        }
+	        contentStream.newLineAtOffset(0, -10);
+        }
+        contentStream.endText();
+        
+        // compétences de la mission
+        contentStream.drawImage(blueStick,565,80,15,180);
+        contentStream.setNonStrokingColor(white);
+        contentStream.addRect(580, 80, 230, 180);
+        contentStream.fill();
+        contentStream.beginText();
+        contentStream.newLineAtOffset(590, 240);
+        contentStream.setFont( font1, 15 );
+        contentStream.setNonStrokingColor(lightblue);
+        contentStream.showText("Compétences mises en avant");
+        contentStream.newLineAtOffset(15, -5);
+        contentStream.setFont( font1, 10 );
+        contentStream.setNonStrokingColor(black);
+        
+        for(Skill skill : project.getSkills()) {
+        	for(String line : separateLines(skill.getLabel(),font1,195,10)) {
+        		contentStream.newLineAtOffset(0, -15);
+		        contentStream.showText(line);
+		    }
+        }
+        contentStream.endText();
+        
+        // Commentaire
+        contentStream.setNonStrokingColor(lightblue);
+        contentStream.beginText();
+        contentStream.newLineAtOffset(60, 240);
+        contentStream.setFont( font2, 15 );
+        contentStream.showText("«");
+        contentStream.newLineAtOffset(0, -5);
+        contentStream.setFont( font1, 10 );
+        String commentary = mission.getLastVersion().getComment();
+        for(String line : separateLines(commentary,font1,220,10)) {
+        	contentStream.newLineAtOffset(0, -15);
+        	contentStream.showText(line);
+        }
+        contentStream.newLineAtOffset(220, -15);
+        contentStream.setFont( font2, 15 );
+        contentStream.showText("»");
+        contentStream.endText();
+        
+        
+        
+        // Note de version
+        contentStream.setNonStrokingColor(black);
+        contentStream.beginText();
+        contentStream.newLineAtOffset(10, 10);
+        contentStream.setFont( font1, 10 );
+        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy"); 
+        contentStream.showText(dateFormat.format(mission.getLastVersion().getVersionDate()));
+        contentStream.endText();
+        
+        // Bulles
+        
+        float constantForCircleWithBezierCurve = (float) 0.551915024494; // voir Bezier Curves 
+        int rayon = 28;
+        int cx1 = 420;
+        int cy1 = 40;
+        int cx2 = 520;
+        int cx3 = 620;
+        int cx4 = 720;
+        
+        
+        // Bulle contrat
+        contentStream.setNonStrokingColor(white);
+        contentStream.moveTo(cx1 - rayon, cy1);
+        contentStream.curveTo(cx1 - rayon, cy1 + constantForCircleWithBezierCurve*rayon, cx1 - constantForCircleWithBezierCurve*rayon, cy1 + rayon, cx1, cy1 + rayon);
+        contentStream.curveTo(cx1 + constantForCircleWithBezierCurve*rayon, cy1 + rayon, cx1 + rayon, cy1 + constantForCircleWithBezierCurve*rayon, cx1 + rayon, cy1);
+        contentStream.curveTo(cx1 + rayon, cy1 - constantForCircleWithBezierCurve*rayon, cx1 + constantForCircleWithBezierCurve*rayon, cy1 - rayon, cx1, cy1 - rayon);
+        contentStream.curveTo(cx1 - constantForCircleWithBezierCurve*rayon, cy1 - rayon, cx1 - rayon, cy1 - constantForCircleWithBezierCurve*rayon, cx1 - rayon, cy1);
+        contentStream.fill();
+        contentStream.drawImage(contractIcon,411,45,contractIcon.getWidth()/3,contractIcon.getHeight()/3);
+        contentStream.setNonStrokingColor(black);
+        contentStream.setFont( font1, 8 );
+        showCenteredText(contentStream,modifyText(mission.getLastVersion().getContractType().name()),cx1);
+        
+        
+        // Bulle équipe 
+        contentStream.setNonStrokingColor(white);
+        contentStream.moveTo(cx2 - rayon, cy1);
+        contentStream.curveTo(cx2 - rayon, cy1 + constantForCircleWithBezierCurve*rayon, cx2 - constantForCircleWithBezierCurve*rayon, cy1 + rayon, cx2, cy1 + rayon);
+        contentStream.curveTo(cx2 + constantForCircleWithBezierCurve*rayon, cy1 + rayon, cx2 + rayon, cy1 + constantForCircleWithBezierCurve*rayon, cx2 + rayon, cy1);
+        contentStream.curveTo(cx2 + rayon, cy1 - constantForCircleWithBezierCurve*rayon, cx2 + constantForCircleWithBezierCurve*rayon, cy1 - rayon, cx2, cy1 - rayon);
+        contentStream.curveTo(cx2 - constantForCircleWithBezierCurve*rayon, cy1 - rayon, cx2 - rayon, cy1 - constantForCircleWithBezierCurve*rayon, cx2 - rayon, cy1);
+        contentStream.fill();
+        contentStream.drawImage(teamIcon,512,45,teamIcon.getWidth()/3,teamIcon.getHeight()/3);
+        contentStream.setNonStrokingColor(black);
+        contentStream.setFont( font1, 8 );
+        showCenteredText(contentStream,modifyText("Taille : "+mission.getLastVersion().getTeamSize().toString()),cx2);
+        
+     // Bulle durée 
+        contentStream.setNonStrokingColor(white);
+        contentStream.moveTo(cx3 - rayon, cy1);
+        contentStream.curveTo(cx3 - rayon, cy1 + constantForCircleWithBezierCurve*rayon, cx3 - constantForCircleWithBezierCurve*rayon, cy1 + rayon, cx3, cy1 + rayon);
+        contentStream.curveTo(cx3 + constantForCircleWithBezierCurve*rayon, cy1 + rayon, cx3 + rayon, cy1 + constantForCircleWithBezierCurve*rayon, cx3 + rayon, cy1);
+        contentStream.curveTo(cx3 + rayon, cy1 - constantForCircleWithBezierCurve*rayon, cx3 + constantForCircleWithBezierCurve*rayon, cy1 - rayon, cx3, cy1 - rayon);
+        contentStream.curveTo(cx3 - constantForCircleWithBezierCurve*rayon, cy1 - rayon, cx3 - rayon, cy1 - constantForCircleWithBezierCurve*rayon, cx3 - rayon, cy1);
+        contentStream.fill();
+        contentStream.drawImage(durationIcon,610,45,durationIcon.getWidth()/3,durationIcon.getHeight()/3);
+        contentStream.setNonStrokingColor(black);
+        contentStream.setFont( font1, 8 );
+        int durationDay = 0;
+        int durationMonth = 0;
+        int durationYear = 0;
+        
+        if(project.getBeginDate()!=null && project.getEndDate()!=null) {
+	        Period period = Period.between(project.getBeginDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate(),
+	        			                       project.getEndDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+	        durationDay =  period.getDays();
+	        durationMonth = period.getMonths();
+	        durationYear = period.getYears();
+        }
+        	
+        
+        
+        showCenteredText(contentStream,modifyText(durationToText(durationDay,durationMonth, durationYear)),cx3);
+        
+     // Bulle localisation
+        contentStream.setNonStrokingColor(white);
+        contentStream.moveTo(cx4 - rayon, cy1);
+        contentStream.curveTo(cx4 - rayon, cy1 + constantForCircleWithBezierCurve*rayon, cx4 - constantForCircleWithBezierCurve*rayon, cy1 + rayon, cx4, cy1 + rayon);
+        contentStream.curveTo(cx4 + constantForCircleWithBezierCurve*rayon, cy1 + rayon, cx4 + rayon, cy1 + constantForCircleWithBezierCurve*rayon, cx4 + rayon, cy1);
+        contentStream.curveTo(cx4 + rayon, cy1 - constantForCircleWithBezierCurve*rayon, cx4 + constantForCircleWithBezierCurve*rayon, cy1 - rayon, cx4, cy1 - rayon);
+        contentStream.curveTo(cx4 - constantForCircleWithBezierCurve*rayon, cy1 - rayon, cx4 - rayon, cy1 - constantForCircleWithBezierCurve*rayon, cx4 - rayon, cy1);
+        contentStream.fill();
+        contentStream.drawImage(localizationIcon,709,45,localizationIcon.getWidth()/3,localizationIcon.getHeight()/3);
+        contentStream.setNonStrokingColor(black);
+        contentStream.setFont( font1, 8 );
+        showCenteredText(contentStream,modifyText(mission.getLastVersion().getCity()+", "+mission.getLastVersion().getCountry()),cx4);
+        
+        contentStream.close();
 		
 	}
 	
