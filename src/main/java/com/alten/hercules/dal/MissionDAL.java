09cb4ab9814bdd8853f48c0f589f126139c5e882
Adrien Bègue <adrien.begue@alten.com>
2020-06-12 09:40:07 +0200

    gestion recherche string consultant & client

--- src/main/java/com/alten/hercules/dal/MissionDAL.java
+++ src/main/java/com/alten/hercules/dal/MissionDAL.java
@@ -101,13 +101,20 @@ public class MissionDAL {
             key = "customer";
 	        if (criteria.containsKey(key) && !criteria.get(key).isBlank())
 	        	criteriaList.add(builder.equal(customerJoin.get("id"), Long.parseLong(criteria.get(key))));
-        } catch (NumberFormatException ignored) {}
+        } catch (NumberFormatException e) {
+        	criteriaList.add(builder.like(builder.lower(customerJoin.get("name")), ("%" + criteria.get(key) + "%").toLowerCase()));
+        }
 	        
         try {
 	        key = "consultant";
 	        if (criteria.containsKey(key) && !criteria.get(key).isBlank())
 	        	criteriaList.add(builder.equal(consultantJoin.get("id"), Long.parseLong(criteria.get(key))));
-        } catch (NumberFormatException ignored) {}
+        } catch (NumberFormatException e) {
+        	final String pattern = ("%" + criteria.get(key) + "%").toLowerCase();
+        	criteriaList.add(builder.or(
+        			builder.like(builder.lower(consultantJoin.get("firstname")), pattern),
+        			builder.like(builder.lower(consultantJoin.get("lastname")), pattern)));
+        }
 
         
         key = "location";
