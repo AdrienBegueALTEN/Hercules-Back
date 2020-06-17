d40c021c8793953703d8bd1543e8906d071e76dd
Adrien Bègue <adrien.begue@alten.com>
2020-06-17 16:04:48 +0200

    Update ResourceNotFoundException.java

--- src/main/java/com/alten/hercules/model/exception/ResourceNotFoundException.java
+++ src/main/java/com/alten/hercules/model/exception/ResourceNotFoundException.java
@@ -3,7 +3,7 @@ package com.alten.hercules.model.exception;
 import org.springframework.http.HttpStatus;
 
 public class ResourceNotFoundException extends ResponseEntityException {
-	public ResourceNotFoundException(Class<?> resource) {
-		super(resource.getName() + " not found.", HttpStatus.NOT_FOUND);
+	public ResourceNotFoundException(Class<?> resourceClass) {
+		super(resourceClass.getName() + " not found.", HttpStatus.NOT_FOUND);
 	}
 }
